88c13fd7446ea6becf6c95cc8e2ba870dc41b76c
Schaepher <279393092@qq.com>
2016-01-21 22:59:11 +0800

    改了变量名，加上注释

--- ZhihuParserWithoutGUI(login).py
+++ ZhihuParserWithoutGUI(login).py
@@ -17,13 +17,11 @@ def write(title, data):
     out.close()
 
 
-def parser(url):
-    import urllib.request
-    from html.parser import HTMLParser
+def parse(html_content):
     import re
 
-    html_bytes = urllib.request.urlopen().read()
-    html = html_bytes.decode('UTF-8')
+    # html_bytes = urllib.request.urlopen(url)
+    # html_content = html_bytes.read().decode('UTF-8')
 
     title = re.compile(
             r'<h2 class="zm-item-title"><a target="_blank" href="/question/\d+">'
@@ -31,10 +29,10 @@ def parser(url):
             r'<div class="zm-item-vote-info " data-votecount="(\d+)">.+?'
             r'<textarea class="content hidden">(.+?)<span.+?</textarea>',
             re.DOTALL)
-    content_array = title.findall(html)
-
-    for i in range(len(content_array)):
-        main_content = HTMLParser().unescape(content_array[i][3])
+    content_array = title.findall(html_content)
+    content_array_len = len(content_array)
+    for i in range(content_array_len):
+        main_content = html_content.unescape(content_array[i][3])
         main_content = re.sub('(<img.+?>)', '<br>\\1<br>', main_content)
         content = '<h2 align="center">' + content_array[i][0] + '</h2><br><span style="float:right;">赞数：' + \
                   content_array[i][2] + '</span><br><span style="float:right;">作者：' + content_array[i][
@@ -61,43 +59,58 @@ def login():
         'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4,zh-TW;q=0.2',
         'Connection': 'keep-alive',
         'Host': 'www.zhihu.com',
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
         'Referer': 'http://www.zhihu.com/'
     }
-    url = 'http://www.zhihu.com/#signin'
 
-        # cookie = http.cookiejar.CookieJar()
-    cookie = http.cookiejar.MozillaCookieJar('cookie.ini')
-    cookie_set = urllib.request.HTTPCookieProcessor(cookie)
-    opener = urllib.request.build_opener(cookie_set)
-    o = opener.open(url)
-    html_bytes = o.read().decode('UTF-8')
+    # 打开保存cookie的文件，并设置cookie
+    cookie_file = http.cookiejar.MozillaCookieJar('cookie.ini')
+    cookie_processor = urllib.request.HTTPCookieProcessor(cookie_file)
+    opener = urllib.request.build_opener(cookie_processor)
+
+    # 获取xsrf，登陆的验证需要这个
+    url = 'http://www.zhihu.com/#signin'
+    html_open = opener.open(url)
+    html_content = html_open.read().decode('UTF-8')
     reg = r'name="_xsrf" value="(.*)"/>'
     pattern = re.compile(reg)
-    result = pattern.findall(html_bytes)
+    result = pattern.findall(html_content)
     xsrf = result[0]
     login_data['_xsrf'] = xsrf
 
+    # 获取验证码
     captcha_url = 'http://www.zhihu.com/captcha.gif'
     captcha = urllib.request.Request(captcha_url, headers=headers_base)
-    opener1 = urllib.request.build_opener(cookie_set)
-    html_bytes = opener1.open(captcha)
-    f = open('captcha.gif', 'wb')
-    f.write(html_bytes.read())
+    html_content = opener.open(captcha)
+    f = open('验证码.gif', 'wb')
+    f.write(html_content.read())
     f.close()
 
-    print('请输入验证码:')
+    # 把验证码放入请求的头部
+    print('请输入验证码（验证码图片在该程序所在的文件夹内）:\n')
     captcha_str = sys.stdin.readline()
     login_data['captcha'] = captcha_str[0:4]
+
+    # 登陆。要对login_data进行编码上的处理，否则会出错
     login_url = 'http://www.zhihu.com/login/email'
     login_data = urllib.parse.urlencode(login_data)
     login_data = login_data.encode('utf-8')
     start_login = urllib.request.Request(login_url, headers=headers_base)
-    opener2 = urllib.request.build_opener(cookie_set)
-    results1 = opener2.open(start_login, login_data)
-    print(results1.read())
-    cookie.save(ignore_discard=True, ignore_expires=True)
+    result = opener.open(start_login, login_data)
+    html_content = result.read().decode('UTF-8')
+
+    # 将返回的字符串转为字典dic
+    result = eval(html_content)
+    result = result.get('msg')
+    print(result)
+    cookie_file.save(ignore_discard=True, ignore_expires=True)
+
+    shoucangjia = 'https://www.zhihu.com/collection/42917965'
+    shoucangjia_content = urllib.request.Request(shoucangjia, headers=headers_base)
+    result = opener.open(shoucangjia_content)
+    html_content = result.read().decode('UTF-8')
+    parse(html_content)
 
 # mkdir()
-# com()
 login()
--- ZhihuParserWithoutGUI.py
+++ ZhihuParserWithoutGUI.py
@@ -1,38 +1,41 @@
-def write(title,data):
-	import os
-	out = open(title + '.html','w',encoding='utf-8')
-	print (data,file=out)
-	out.close
-	
+def write(title, data):
+    import os
+    out = open(title + '.html', 'w', encoding='utf-8')
+    print(data, file=out)
+    out.close
+
+
 def parser(url):
-	import urllib.request
-	import re
-	
-	htmlBytes = urllib.request.urlopen(url).read()
-	html = htmlBytes.decode('UTF-8')
-	
-	title = re.compile(r'<h2.+?>\s+(.+?)\s+</h2>',re.DOTALL)
-	titleArray = title.findall(html)
-	
-	voteNum = re.compile(r'<div class="zm-item-vote-info " data-votecount="(\d+)"',re.DOTALL)
-	voteArray = voteNum.findall(html)
-	
-	name = re.compile(r'<a class="author-link" data-tip=".+?>(.+?)</a>',re.DOTALL)
-	nameArray = name.findall(html)
-	
-	answer = re.compile(r'<div class="zm-editable-content clearfix">.+?</div>',re.DOTALL)
-	answerArray = answer.findall(html)
-	
-	content = '<h2 align="center">'+titleArray[0]+'</h2><br><span style="float:right;">赞数：'+voteArray[0]+'</span><br><span style="float:right;">作者：'+nameArray[0]+'</span><br><br>'+answerArray[0]
-	
-	write(titleArray[0],content)
-	
+    import urllib.request
+    import re
+
+    htmlBytes = urllib.request.urlopen(url).read()
+    html = htmlBytes.decode('UTF-8')
+
+    title = re.compile(r'<h2.+?>\s+(.+?)\s+</h2>', re.DOTALL)
+    titleArray = title.findall(html)
+
+    voteNum = re.compile(r'<div class="zm-item-vote-info " data-votecount="(\d+)"', re.DOTALL)
+    voteArray = voteNum.findall(html)
+
+    name = re.compile(r'<a class="author-link" data-tip=".+?>(.+?)</a>', re.DOTALL)
+    nameArray = name.findall(html)
+
+    answer = re.compile(r'<div class="zm-editable-content clearfix">.+?</div>', re.DOTALL)
+    answerArray = answer.findall(html)
+
+    content = '<h2 align="center">' + titleArray[0] + '</h2><br><span style="float:right;">赞数：' + voteArray[
+        0] + '</span><br><span style="float:right;">作者：' + nameArray[0] + '</span><br><br>' + answerArray[0]
+
+    write(titleArray[0], content)
+
 
 def com():
-	import sys
-	print("请输入知乎问题页面的URL")
-	print("例如：http://www.zhihu.com/question/26125708")
-	line = sys.stdin.readline()
-	parser(line)
-	
+    import sys
+    print("请输入知乎问题页面的URL")
+    print("例如：http://www.zhihu.com/question/26125708")
+    line = sys.stdin.readline()
+    parser(line)
+
+
 com()
